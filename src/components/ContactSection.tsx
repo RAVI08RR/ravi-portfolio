52b73b895f0bd1e4a0dd42cf89d73f1705a3b5a3
RAVI08RR <100826426+RAVI08RR@users.noreply.github.com>
2025-11-28 22:14:50 +0530

    Update email address in ContactSection component

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -54,7 +54,9 @@ const ContactSection = () => {
                 <svg className="w-5 h-5 mt-0.5 mr-3 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <span>ravi.belpade@example.com</span>
+<span>
+  <a href="mailto:ravisoni08rrrr@gmail.com">ravisoni08rrrr@gmail.com</a>
+</span>
               </li>
               <li className="flex items-start">
                 <svg className="w-5 h-5 mt-0.5 mr-3 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
